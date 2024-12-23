0e7ae0af654c495dbe3d3ba6c4e25be283fe6982
Mayuresh Kadu <mayuresh.kadu@kainos.com>
2024-12-23 14:47:10 +0000

    ollama-chat/: Added

---
+++ ollama-chat/README.md
@@ -0,0 +1,3 @@
+# Chatting with Llama 3.2 running locally on Ollama
+
+All this does is connects to the local LLM and invokes it's [chat completion API](https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-chat-completion)
---
+++ ollama-chat/ollama-chat.py
@@ -0,0 +1,24 @@
+"""Basic script to use the "chat completion" API of Llama 3.12 model via Ollama. 
+   See: https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-chat-completion
+"""
+
+from ollama import ChatResponse, chat
+
+LLM_NAME = "llama3.2"  # The LLM to use
+CHAT_PROMPT = "Who is Linus Torvalds?"  # What I want to ask the LLM
+
+try:
+    response: ChatResponse = chat(
+        model=LLM_NAME,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant. Be as accurate and brief as possible."},
+            {"role": "user", "content": CHAT_PROMPT},
+        ],
+        # stream=False # No need - this is false by default
+    )
+
+    print(f"The LLM ({LLM_NAME}) responded with the following:")
+    print(response["message"]["content"])
+
+except Exception as e:
+    print('There was an Error:', e.error)
