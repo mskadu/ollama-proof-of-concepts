55c8b9647b5e22dfe2b2af6ed9c9db770addfeaa
Mayuresh Kadu <mayuresh.kadu@kainos.com>
2024-12-24 12:23:21 +0000

    tool-calling: Added weather API

--- tool-calling/tool_calling.py
+++ tool-calling/tool_calling.py
@@ -1,13 +1,23 @@
 """PoC for implementing tool-calling with Ollama3.2"""
 import ollama
+import requests
 
 LLM_NAME = "llama3.2"  # The generative LLM
 
 
 def get_current_weather(city):
-    """dummy function to get current weather"""
-    # returning a mock response
-    return f"The current weather in {city} is sunny with a temperature of 31°C."
+    """Get the current weather for a city using a free API"""
+    url = f"http://wttr.in/{city}?format=3"
+    try:
+        weather_response = requests.get(url, timeout=5)
+        if weather_response.status_code == 200:
+            city_weather = weather_response.text.split(':')[1].strip()
+            return f"The current weather in {city} is {city_weather}."
+        else:
+            return f"Could not retrieve weather data for {city}."
+
+    except requests.exceptions.RequestException as e:
+        return f"An error occurred while retrieving weather data: {e}"
 
 
 # The tool/ function to be used by our LLM
@@ -35,11 +45,12 @@ weather_tool = {
 # the output changes
 ##
 
-# PROMPT_TEXT = "What is the weather in Jakarta?"
-PROMPT_TEXT = "How hot is it in California today?"
-# PROMPT_TEXT = "Will I need an umbrella today?" # note how there is no city?
+PROMPT_TEXT = "What is the weather in Jakarta?"
+# PROMPT_TEXT = "How hot is it in California today?"
+# PROMPT_TEXT = "Will I need an umbrella today?"  # note how there is no city?
 
 # note this is irrelevant to our tool/ function
+# So you should see a "City not found in tool call arguments" message
 # PROMPT_TEXT = "Who is Mahatma Gandhi?"
 
 response = ollama.chat(
@@ -47,13 +58,10 @@ response = ollama.chat(
     messages=[{"role": "user", "content": PROMPT_TEXT}],
     tools=[weather_tool],
 )
-# Peek at the response to see what we've got.
-# print(response)
 
 # Check for tool calls in the response
 if "tool_calls" in response["message"]:
     tool_call = response["message"]["tool_calls"][0]
-    # print("tool_call: ", tool_call)
 
     # Check for city in function arguments
     if "city" in tool_call.function.arguments:
